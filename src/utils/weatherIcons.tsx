0cad0ceb8aff1789842183fa3397267ed76a8010
もっくん <mokkunpc@gmail.com>
2026-01-19 21:52:26 +0900

    fix: rain icon

--- src/utils/weatherIcons.tsx
+++ src/utils/weatherIcons.tsx
@@ -95,23 +95,7 @@ export function getWeatherIcon(
     (weatherCode >= 500 && weatherCode <= 531)
   ) {
     return (
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width={size}
-        height={size}
-        viewBox="0 0 24 24"
-        fill="none"
-        stroke={color}
-        strokeWidth="2"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      >
-        <path d="M16 13v8a1 1 0 0 1-1 1H1a1 1 0 0 1-1-1v-8" />
-        <path d="M23 13v8a1 1 0 0 1-1 1H8a1 1 0 0 1-1-1v-8" />
-        <path d="M8 13l2-9h8l2 9" />
-        <path d="M7 13H1" />
-        <path d="M23 13h-6" />
-      </svg>
+<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-cloud-drizzle-icon lucide-cloud-drizzle"><path d="M4 14.899A7 7 0 1 1 15.71 8h1.79a4.5 4.5 0 0 1 2.5 8.242"/><path d="M8 19v1"/><path d="M8 14v1"/><path d="M16 19v1"/><path d="M16 14v1"/><path d="M12 21v1"/><path d="M12 16v1"/></svg>
     );
   }
   // 600-622: 雪
